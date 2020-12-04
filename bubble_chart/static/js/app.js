105066e9258317796b913d93b0c721e64555c07d
Hunter4 <stuhunter4@users.noreply.github.com>
2020-12-04 05:02:20 -0800

    fixed the link to the nyt; finalized hovertext and labeling for the bubble chart, looking good

--- bubble_chart/static/js/app.js
+++ bubble_chart/static/js/app.js
@@ -1,7 +1,13 @@
 var data = [{
     x: data.map(row => row["Population per square mile"]),
     y: data.map(row => row["Bachelor's degree or higher, percent of persons age 25+"]),
+    hovertemplate: "<b>%{text} County</b><br>" + 
+                    "<b>Bachelor's degree+</b>: %{y}%<br>" + 
+                    "<b>Population density</b>: %{x}<br>" + 
+                    "<b>Population</b>: %{marker.size:,}<br>" + 
+                    "<extra></extra>",
     text: data.map(row => row["County"]),
+    showlegend: false,
     mode: 'markers',
     marker: {
         color: data.map(row => row["2016 Winner"]),
@@ -20,7 +26,19 @@ var data = [{
 
 var layout = {
     xaxis: {
+        title: "POPULATION PER SQUARE MILE",
+        titlefont: {
+        family: 'Arial, sans-serif',
+        size: 14,
+        color: 'grey' },
         type: 'log'
+    },
+    yaxis: {
+      title: "BACHELOR'S DEGREE OR HIGHER, %",
+      titlefont: {
+        family: 'Arial, sans-serif',
+        size: 14,
+        color: 'grey'}
     }
 }
 
