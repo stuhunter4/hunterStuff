b251fd7fe2794246c65a8a2fb9753310396b314b
Hunter4 <stuhunter4@users.noreply.github.com>
2021-03-20 16:45:41 -0700

    added a second bubble chart to the politics&population page, for 2020 data points; really it just adjusts the color to match 2020

--- bubble_chart/static/js/app.js
+++ bubble_chart/static/js/app.js
@@ -1,4 +1,4 @@
-var data = [{
+var data1 = [{
     x: data.map(row => row["Population per square mile"]),
     y: data.map(row => row["Bachelor's degree or higher, percent of persons age 25+"]),
     hovertemplate: "<b>%{text} County</b><br>" + 
@@ -23,8 +23,38 @@ var data = [{
           ]
         }]
 }];
+var data2 = [{
+  x: data2.map(row => row["Population per square mile"]),
+  y: data2.map(row => row["Bachelor's degree or higher, percent of persons age 25+"]),
+  hovertemplate: "<b>%{text} County</b><br>" + 
+                  "<b>Bachelor's degree+</b>: %{y}%<br>" + 
+                  "<b>Population density</b>: %{x}<br>" + 
+                  "<b>Population</b>: %{marker.size:,}<br>" + 
+                  "<extra></extra>",
+  text: data2.map(row => row["County"]),
+  showlegend: false,
+  mode: 'markers',
+  marker: {
+      color: data2.map(row => row["2016 Winner"]),
+      size: data2.map(row => row["Population"]),
+      sizemode: 'area',
+      sizeref:  700
+  },
+  transforms: [
+      {
+        type: 'aggregate',
+        aggregations: [
+          {target: 'marker.size', func: 'sum'}
+        ]
+      }]
+}];
 
-var layout = {
+var layout1 = {
+    title: "2016 U.S. Election Results",
+    //showlegend: false,
+    hovermode: "closest",
+    paper_bgcolor: 'rgba(245,246,249,1)',
+    plot_bgcolor: 'rgba(245,246,249,1)',
     xaxis: {
         title: "POPULATION PER SQUARE MILE",
         titlefont: {
@@ -41,7 +71,30 @@ var layout = {
         color: 'grey'}
     }
 }
+var layout2 = {
+  title: "2020 U.S. Election Results",
+  //showlegend: false,
+  hovermode: "closest",
+  paper_bgcolor: 'rgba(245,246,249,1)',
+  plot_bgcolor: 'rgba(245,246,249,1)',
+  xaxis: {
+      title: "POPULATION PER SQUARE MILE",
+      titlefont: {
+      family: 'Arial, sans-serif',
+      size: 14,
+      color: 'grey' },
+      type: 'log'
+  },
+  yaxis: {
+    title: "BACHELOR'S DEGREE OR HIGHER, %",
+    titlefont: {
+      family: 'Arial, sans-serif',
+      size: 14,
+      color: 'grey'}
+  }
+}
 
 var config = {responsive: true}
 
-Plotly.newPlot('bubble', data, layout, config)
+Plotly.newPlot('bubble', data1, layout1, config)
+Plotly.newPlot('bubble2', data2, layout2, config)
