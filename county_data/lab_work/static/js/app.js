204b1d4331ff8830c2911abe67343718041a0ff6
Hunter4 <stuhunter4@users.noreply.github.com>
2022-01-04 18:37:58 -0800

    completed lab work for covid cases and deaths

--- county_data/lab_work/static/js/app.js
+++ county_data/lab_work/static/js/app.js
@@ -1,6 +1,5 @@
 // from data.js, assign data to a descriptive variable
 var tableData = data;
-//var vaccines = vaccines;
 
 // create graphs and table upon opening page
 // function is at the bottom, a reappropriation of runEnter
@@ -28,42 +27,21 @@ function runEnter() {
     console.log(tableData);
 
     // filter() uses input as its argument
-    var filteredData = tableData.filter(ca => ca.County === inputValue);
+    var filteredData = tableData.filter(ca => ca.county === inputValue);
     // filter() to create dataset for California only
-    var caliData = tableData.filter(ca => ca.County === "California");
+    var caliData = tableData.filter(ca => ca.county === "California");
     // test
     console.log(filteredData);
 
-    // create a reference to the table body
-    var tbody = d3.select("tbody");
-    // remove any children from the table
-    tbody.html("");
-
-    // loop through filtered data and console.log each report object
-    filteredData.forEach(function(caReport) {
-        console.log(caReport);
-        // append one table row 'tr' for each report object
-        var row = tbody.append("tr");
-
-        // use 'Object.entries' to console.log each report value
-        Object.entries(caReport).forEach(function([key, value]) {
-            console.log(key, value);
-            // use d3 to append 1 cell for each value in the report object (column)
-            var cell = row.append("td");
-            // use d3 to update each cell's text with report values
-            cell.text(value);
-        });
-    });
-
     // use the map method with the arrow function to return all the filtered week numbers.
-    var weeks = filteredData.map(county => county["week number"]);
+    var weeks = filteredData.map(county => county["date"]);
 
     // use the map method with the arrow function to return all the filtered average cases per 100,000.
-    var rate = filteredData.map(county => county["cases per cap"]);
-    var cali_rate = caliData.map(county => county["cases per cap"]);
+    var rate = filteredData.map(county => county["cases_per_cap"]);
+    var cali_rate = caliData.map(county => county["cases_per_cap"]);
     // ..and for deaths per capita
-    var drate = filteredData.map(county => county["deaths per cap"]);
-    var cali_drate = caliData.map(county => county["deaths per cap"]);
+    var drate = filteredData.map(county => county["deaths_per_cap"]);
+    var cali_drate = caliData.map(county => county["deaths_per_cap"]);
 
     // create traces
     var trace1 = {
@@ -109,13 +87,13 @@ function runEnter() {
 
     // define our plot layouts
     var layout = {
-        title: "Seven-Day Average COVID-19 Cases",
+        title: "Daily Cases - 7-Day Moving Average",
         paper_bgcolor: 'rgba(245,246,249,1)',
         plot_bgcolor: 'rgba(245,246,249,1)',
-        xaxis: { title: "Weeks: February 1, 2020 - December 31, 2021" },
-        yaxis: { title: "Daily Rate per 100,000"},
+        xaxis: { title: `${weeks[0]} - ${weeks[weeks.length - 1]}`},
+        yaxis: { title: "Per 100,000"},
         yaxis2: {
-            title: "Daily Rate per 100,000",
+            title: "Per 100,000",
             titlefont: {color: 'rgb(148, 103, 189)'},
             tickfont: {color: 'rgb(148, 103, 189)'},
             overlaying: 'y',
@@ -124,13 +102,13 @@ function runEnter() {
     };
 
     var layout2 = {
-        title: "Seven-Day Average COVID-19 Deaths",
+        title: "Daily Deaths - 7-Day Moving Average",
         paper_bgcolor: 'rgba(245,246,249,1)',
         plot_bgcolor: 'rgba(245,246,249,1)',
-        xaxis: { title: "Weeks: February 1, 2020 - December 31, 2021" },
-        yaxis: { title: "Daily Deaths per 100,000"},
+        xaxis: { title: `${weeks[0]} - ${weeks[weeks.length - 1]}`},
+        yaxis: { title: "Per 100,000"},
         yaxis2: {
-            title: "Daily Deaths per 100,000",
+            title: "Per 100,000",
             titlefont: {color: 'rgb(148, 103, 189'},
             tickfont: {color: 'rgb(148, 103, 189'},
             overlaying: 'y',
@@ -151,50 +129,23 @@ function runStart() {
     var firstCounty = "Alameda";
 
     // filter() uses Alameda, the first county, as its argument
-    var filteredData = tableData.filter(ca => ca.County === firstCounty);
+    var filteredData = tableData.filter(ca => ca.county === firstCounty);
     // filter() to create dataset for California only
-    var caliData = tableData.filter(ca => ca.County === "California");
-    // vaccine data
-    //var vaxData = vaccines.filter(ca => ca.county === firstCounty);
-    //var calivaxData = vaccines.filter(ca => ca.county === "California");
+    var caliData = tableData.filter(ca => ca.county === "California");
 
     // test
     console.log(filteredData);
 
-    // create a reference to the table body
-    var tbody = d3.select("tbody");
-    // remove any children from the table
-    tbody.html("");
-
-    // loop through filtered data and console.log each report object
-    filteredData.forEach(function(caReport) {
-        console.log(caReport);
-        // append one table row 'tr' for each report object
-        var row = tbody.append("tr");
-
-        // use 'Object.entries' to append cells and text
-        Object.entries(caReport).forEach(function([key, value]) {
-            // use d3 to append 1 cell for each value in the report object (column)
-            var cell = row.append("td");
-            // use d3 to update each cell's text with report values
-            cell.text(value);
-        });
-    });
-
     // use the map method with the arrow function to return all the filtered week numbers.
-    var weeks = filteredData.map(county => county["week number"]);
+    var weeks = filteredData.map(county => county["date"]);
+    console.log(weeks);
 
     // use the map method with the arrow function to return all the filtered average cases per 100,000.
-    var rate = filteredData.map(county => county["cases per cap"]);
-    var cali_rate = caliData.map(county => county["cases per cap"]);
+    var rate = filteredData.map(county => county["cases_per_cap"]);
+    var cali_rate = caliData.map(county => county["cases_per_cap"]);
     // ..and for deaths per capita
-    var drate = filteredData.map(county => county["deaths per cap"]);
-    var cali_drate = caliData.map(county => county["deaths per cap"]);
-    // data for bar charts
-    //var county_vax = vaxData.map(county => county["percent_full"]);
-    //var cali_vax = calivaxData.map(county => county["percent_full"]);
-    //console.log(county_vax[0]);
-    //console.log(cali_vax[0]);
+    var drate = filteredData.map(county => county["deaths_per_cap"]);
+    var cali_drate = caliData.map(county => county["deaths_per_cap"]);
 
     // create traces
     var trace1 = {
@@ -239,13 +190,13 @@ function runStart() {
 
     // define our plot layouts
     var layout = {
-        title: "Seven-Day Average COVID-19 Cases",
+        title: "Daily Cases - 7-Day Moving Average",
         paper_bgcolor: 'rgba(245,246,249,1)',
         plot_bgcolor: 'rgba(245,246,249,1)',
-        xaxis: {title: "Weeks: February 1, 2020 - December 31, 2021" },
-        yaxis: {title: "Daily Rate per 100,000"},
+        xaxis: {title: `${weeks[0]} - ${weeks[weeks.length - 1]}`},
+        yaxis: {title: "Per 100,000"},
         yaxis2: {
-            title: "Daily Rate per 100,000",
+            title: "Per 100,000",
             titlefont: {color: 'rgb(148, 103, 189)'},
             tickfont: {color: 'rgb(148, 103, 189)'},
             overlaying: 'y',
@@ -254,13 +205,13 @@ function runStart() {
     };
 
     var layout2 = {
-        title: "Seven-Day Average COVID-19 Deaths",
+        title: "Daily Deaths - 7-Day Moving Average",
         paper_bgcolor: 'rgba(245,246,249,1)',
         plot_bgcolor: 'rgba(245,246,249,1)',
-        xaxis: {title: "Weeks: February 1, 2020 - December 31, 2021" },
-        yaxis: {title: "Daily Deaths per 100,000"},
+        xaxis: {title: `${weeks[0]} - ${weeks[weeks.length - 1]}`},
+        yaxis: {title: "Per 100,000"},
         yaxis2: {
-            title: "Daily Deaths per 100,000",
+            title: "Per 100,000",
             titlefont: {color: 'rgb(148, 103, 189'},
             tickfont: {color: 'rgb(148, 103, 189'},
             overlaying: 'y',
